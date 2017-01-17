d7f93dd39dba2539cef0b9f916e6816eadbb0af0
julien-gazzola <julien.gazzola@epitech.eu>
2017-01-17 15:17:27 +0100

    settings changement de size + debut couleur

--- js/controllers/settings.js
+++ js/controllers/settings.js
@@ -10,12 +10,21 @@
 var app = angular.module('naview');
 
 function SettingsController($scope, $http, API, $location) {
-  $scope.logout = function () {
-    $location.url("/");
+  $scope.smallScreen = function () {
+    self.resizeTo(Math.floor(screen.width / 1.7), Math.floor(screen.height / 1.7));
   };
-  $scope.locate = function () {
-    $location.url("homepage");
-  }
+
+  $scope.defaultScreen = function () {
+    self.resizeTo(Math.floor(screen.width / 1.2), Math.floor(screen.height / 1.2));
+
+  };
+  $scope.largeScreen = function () {
+    self.resizeTo(Math.floor(screen.width / 1), Math.floor(screen.height / 1));
+
+  };
+  $scope.changeColor = function () {
+    document.getElementById("body").style.backgroundColor="red";
+  };
 };
 
 app.controller('SettingsController', ['$scope', '$http', 'API', '$location' , SettingsController]);
