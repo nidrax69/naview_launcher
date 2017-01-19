a55b6497306479d7360466d1d53ae1f089a31711
julien-gazzola <julien.gazzola@epitech.eu>
2017-01-19 12:47:32 +0100

    setting :modif size + début couleur

--- js/controllers/settings.js
+++ js/controllers/settings.js
@@ -10,20 +10,11 @@
 var app = angular.module('naview');
 
 function SettingsController($scope, $http, API, $location) {
-  $scope.smallScreen = function () {
-    self.resizeTo(Math.floor(screen.width / 1.7), Math.floor(screen.height / 1.7));
+  $scope.screen = function (a) {
+    self.resizeTo(Math.floor(screen.width / a), Math.floor(screen.height / a));
   };
-
-  $scope.defaultScreen = function () {
-    self.resizeTo(Math.floor(screen.width / 1.2), Math.floor(screen.height / 1.2));
-
-  };
-  $scope.largeScreen = function () {
-    self.resizeTo(Math.floor(screen.width / 1), Math.floor(screen.height / 1));
-
-  };
-  $scope.changeColor = function () {
-    document.getElementById("body").style.backgroundColor="red";
+  $scope.changeColor = function (a) {
+    document.getElementById("body").style.backgroundColor = a;
   };
 };
 
