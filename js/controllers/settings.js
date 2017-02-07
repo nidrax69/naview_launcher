d7e228babb54e8246d728b5381133f16db7964d2
julien-gazzola <julien.gazzola@epitech.eu>
2017-02-07 19:46:04 +0100

    ajout palette couleur + slider volume

--- js/controllers/settings.js
+++ js/controllers/settings.js
@@ -13,9 +13,32 @@ function SettingsController($scope, $http, API, $location) {
   $scope.screen = function (a) {
     self.resizeTo(Math.floor(screen.width / a), Math.floor(screen.height / a));
   };
-  $scope.changeColor = function (a) {
+  $scope.changeColor = function(a) {
     document.getElementById("body").style.backgroundColor = a;
   };
+
+  function play(idPlayer, control) {
+    var player = document.querySelector('#' + idPlayer);
+
+    if (player.paused) {
+        player.play();
+        control.textContent = 'Pause';
+    } else {
+        player.pause();
+        control.textContent = 'Play';
+    }
+}
+
+function resume(idPlayer) {
+    var player = document.querySelector('#' + idPlayer);
+
+    player.currentTime = 0;
+    player.pause();
+}
+  // $scope.testSound = fonction(){
+  //   var audio = new Audio('Tetris.mp3');
+  //   audio.play();
+  // }
 };
 
 app.controller('SettingsController', ['$scope', '$http', 'API', '$location' , SettingsController]);
