fc0b0f418b8927e7130a4f14dfa1c4ca58ea0035
nidrax <v.floriot@hotmail.fr>
2017-03-31 20:01:33 +0200

    Integration API heroku

--- js/constants.js
+++ js/constants.js
@@ -1,6 +1,6 @@
 var app = angular.module('naview');
 
 app.factory('API', function($location){
-    var api = "http://127.0.0.1:3000"
+    var api = "http://naview-server.herokuapp.com"
     return api;
 })
